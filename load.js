5cab95d4732aa7e92b53c073fbb1c24d86f4e612
happy <68935981+youneedpython@users.noreply.github.com>
2023-06-14 14:58:34 +0900

    Update load.js

--- load.js
+++ load.js
@@ -1,8 +1,1 @@
-console.log('load.js 실행 시작');
-const body = document.querySelector('body');
-
-const h1 = document.createElement('h1');
-h1.innerText = '제목입니다';
-body.appendChild(h1);
-
 console.log('load.js 실행 완료');
